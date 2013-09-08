b69fa0f0c0bf9e662112bd5f50efd4edb4ea1c3a
snowdream <yanghui1986527@gmail.com>
2013-09-09 00:21:10 +0800

    [Add] AsyncTask
    Signed-off-by: snowdream <yanghui1986527@gmail.com>

--- src/main/java/com/github/snowdream/android/util/TaskListener.java
+++ src/main/java/com/github/snowdream/android/util/TaskListener.java
@@ -1,15 +1,49 @@
+
 package com.github.snowdream.android.util;
 
-public class TaskListener<Result> {
-	public void onStart() {
-	};
+public class TaskListener<Progress, Result> {
+    /**
+     * Runs on the UI thread before doInBackground(Params...).
+     */
+    public void onStart() {
+    };
 
-	public void onSuccess(Result result) {
-	};
+    /**
+     * Runs on the UI thread after publishProgress(Progress...) is invoked. The
+     * specified values are the values passed to publishProgress(Progress...).
+     * 
+     * @param values The values indicating progress.
+     */
+    public void onProgressUpdate(Progress... values) {
+    };
 
-	public void onError(Throwable thr) {
-	};
+    /**
+     * Runs on the UI thread after doInBackground(Params...). The specified
+     * result is the value returned by doInBackground(Params...). This method
+     * won't be invoked if the task was cancelled.
+     * 
+     * @param result The result of the operation computed by
+     *            doInBackground(Params...).
+     */
+    public void onSuccess(Result result) {
+    };
 
-	public void onFinish() {
-	};
+    /**
+     * Applications should preferably override onCancelled(Object). This method
+     * is invoked by the default implementation of onCancelled(Object). Runs on
+     * the UI thread after cancel(boolean) is invoked and
+     * doInBackground(Object[]) has finished.
+     */
+    public void onCancelled() {
+    };
+
+    public void onError(Throwable thr) {
+    };
+
+    /**
+     * Runs on the UI thread after doInBackground(Params...) when the task is
+     * finished or cancelled.
+     */
+    public void onFinish() {
+    };
 }
