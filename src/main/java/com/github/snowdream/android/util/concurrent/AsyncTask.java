6a8c06bfed40874abea8b83f6d62bed67f380505
snowdream <yanghui1986527@gmail.com>
2013-09-09 11:20:16 +0800

    [Add]1、modify the package name and the groupid to com.github.snowdream.android.util.concurrent

--- src/main/java/com/github/snowdream/android/util/concurrent/AsyncTask.java
+++ src/main/java/com/github/snowdream/android/util/concurrent/AsyncTask.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  *******************************************************************************/
 
-package com.github.snowdream.android.util;
+package com.github.snowdream.android.util.concurrent;
 
 import android.annotation.TargetApi;
 import android.os.Build;
