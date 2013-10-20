dfb9e0339906cdbdf872822547fcf4f06ef29a91
snowdream <yanghui1986527@gmail.com>
2013-10-21 00:17:33 +0800

    [Update] maven mirrate to gradle

---
+++ build/source/buildConfig/debug/com/github/snowdream/android/util/concurrent/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.github.snowdream.android.util.concurrent;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
